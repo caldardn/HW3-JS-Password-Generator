fd44e06a39bb928312f678efcce1d596c5b34bc5
caldardn <caldardn@gmail.com>
2023-09-01 12:38:00 -0400

    Adding if statment that will prompt the user what they would like for their password

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HW3-JS-Password-Generator
+
+## credits
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#instance_methods
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = upperCase.toLowerCase();
+var numeric = "1234567890";
+var special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
+
+
+
+alert("Welcome to DNC's password generator!!")
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+
+function generatePassword(){
+  var usersChoices = ""
+  var thePassWord = ""
+  var length = prompt("Please enter the character length of your password (8-128)")
+
+  if(length >=8 && length <= 128){
+    var upperCase = confirm("Would you like Uppercase letters in your password?")
+    var lowerCase = confirm("Would you like Lowercase letters in your password?")
+    var numeric = confirm("Would you like Numbers in your password?")
+    var special = confirm("Finally would you like Special characters in your password")
+  }
+}
+
+
+
 generateBtn.addEventListener("click", writePassword);
