2d2132acfdd474ee0697392393d1dbc63e3561fb
caldardn <caldardn@gmail.com>
2023-09-01 13:15:19 -0400

    Adding if statment that will merge the users choosen arrays together and an alert statment is an input was incorrect

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,7 @@ var lowerCase = upperCase.toLowerCase();
 var numeric = "1234567890";
 var special = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
 
-
-
-alert("Welcome to DNC's password generator!!")
-
-
+alert("Welcome to DNC's password generator!!");
 
 // Write password to the #password input
 function writePassword() {
@@ -20,20 +16,42 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  var usersChoices = "";
+  var thePassWord = "";
+  var length = prompt(
+    "Please enter the character length of your password (8-128)"
+  );
 
-function generatePassword(){
-  var usersChoices = ""
-  var thePassWord = ""
-  var length = prompt("Please enter the character length of your password (8-128)")
+  if (length >= 8 && length <= 128) {
+    var upperCase = confirm(
+      "Would you like Uppercase letters in your password?"
+    );
+    var lowerCase = confirm(
+      "Would you like Lowercase letters in your password?"
+    );
+    var numeric = confirm("Would you like Numbers in your password?");
+    var special = confirm(
+      "Finally, would you like Special characters in your password"
+    );
 
-  if(length >=8 && length <= 128){
-    var upperCase = confirm("Would you like Uppercase letters in your password?")
-    var lowerCase = confirm("Would you like Lowercase letters in your password?")
-    var numeric = confirm("Would you like Numbers in your password?")
-    var special = confirm("Finally would you like Special characters in your password")
+    if (upperCase === true) {
+      usersChoices = usersChoices.concat(upperCase);
+    }
+    if (lowerCase === true) {
+      usersChoices = usersChoices.concat(lowerCase);
+    }
+    if (numeric === true) {
+      usersChoices = usersChoices.concat(numeric);
+    }
+    if (special === true) {
+      usersChoices = usersChoices.concat(special);
+    } else {
+    }
+  } else {
+    alert("Invaild response, please try again.");
   }
 }
-
 
 
 generateBtn.addEventListener("click", writePassword);
