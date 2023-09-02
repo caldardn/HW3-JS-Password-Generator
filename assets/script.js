676a3814e412c6821f45cb766be400671e5cdf54
caldardn <caldardn@gmail.com>
2023-09-02 10:40:30 -0400

    Updating JavaScript to have main variables in an array as properties in that array

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,15 @@
 // Assignment Coden
 var generateBtn = document.querySelector("#generate");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = upperCase.toLowerCase();
-var numeric = "1234567890";
-var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var passCriteria = {
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lower:"abcdefghijklmnopqrstuvwxyz",
+  num: "1234567890",
+  spec: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+};
 
-// alert("Welcome to DNC's password generator!!");
+
+
+alert("Welcome to DNC's password generator!!");
 
 // Write password to the #password input
 function writePassword() {
@@ -18,44 +22,44 @@ function writePassword() {
 function generatePassword() {
   var usersChoices = "";
   var passWord = "";
-  var lenght = window.prompt(
+  var length = window.prompt(
     "Please enter the character length of your password (8-128)"
   );
 
-  if (lenght >= 8 && lenght <= 128) {
+  if (length >= 8 && length <= 128) {
     var chosenUpper = window.confirm(
       "Would you like Uppercase letters in your password?"
     );
     var chosenLower = window.confirm(
       "Would you like Lowercase letters in your password?"
     );
-    var chosenNum = window.confirm("Would you like Numbers in your password?"
+    var chosenNum = window.confirm(
+      "Would you like Numbers in your password?"
     );
     var chosenSpec = window.confirm(
       "Finally, would you like Special characters in your password"
     );
 
     if (chosenUpper === true) {
-      usersChoices = usersChoices.concat(upperCase);
+      usersChoices = usersChoices.concat(passCriteria.upper);
     }
     if (chosenLower === true) {
-      usersChoices = usersChoices.concat(lowerCase);
+      usersChoices = usersChoices.concat(passCriteria.lower);
     }
     if (chosenNum === true) {
-      usersChoices = usersChoices.concat(numeric);
+      usersChoices = usersChoices.concat(passCriteria.num);
     }
     if (chosenSpec === true) {
-      usersChoices = usersChoices.concat(special);
+      usersChoices = usersChoices.concat(passCriteria.spec);
     } else {
-      
     }
   } else {
     alert("Invalid response, please try again.");
   }
 
-  for (var i = 0; i < lenght; i++) {
+  for (var i = 0; i < length; i++) {
     var passString = Math.floor(Math.random() * usersChoices.length);
-    passWord = passWord + usersChoices[passString];
+    passWord += usersChoices[passString];
   }
 
   return passWord;
